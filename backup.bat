36845288744b86286f5f76322d73030c4f59e47e
Debs Daitani <debs@daitani.co.uk>
2025-11-27 21:40:15 +0000

    Auto-backup

---
+++ backup.bat
@@ -0,0 +1,14 @@
+@echo off
+cd "C:\Users\debsw\OneDrive - Debs Daitani Ltd\Documents\Daitani\SN Build\GitHub\daitaniverse-build"
+git add .
+git commit -m "Auto-backup"
+git push
+echo BACKUP COMPLETE!
+pause
+```
+
+5. Press **Ctrl+S** to save
+
+**Then in terminal:**
+```
+backup.bat
